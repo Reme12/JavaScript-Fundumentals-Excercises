968f292d46f249a60f852c114ca98a59baf12523
Reme12 <100137126+Reme12@users.noreply.github.com>
2022-09-07 14:35:20 +0200

    Create Assignments.js

---
+++ Assignments.js
@@ -0,0 +1,11 @@
+//Assignment No.1
+/*Assignment Instructions:1. Declare variables called 'country', 'continent' and 'population' and
+assign their values according to your own country (population in millions)
+2. Log their values to the console
+*/
+
+const Country = 'Germany';
+const Continent = 'Europe';
+const Population = 81000000;
+
+console.log(Country, Continent, Population);
